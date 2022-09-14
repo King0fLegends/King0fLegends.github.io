12e580e2c31e64889df23490029e04316312d01e
XuanSon99 <nguyensonn124@gmail.com>
2022-09-15 02:21:09 +0700

    change

---
+++ js/chunk-2d0c73fe.0804d836.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c73fe"],{"508e":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("main",{staticClass:"pd-50"})},s=[],a={},l=a,u=t("2877"),i=Object(u["a"])(l,c,s,!1,null,null,null);e["default"]=i.exports}}]);
